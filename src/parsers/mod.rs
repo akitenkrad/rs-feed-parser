dedfb15e463b0ff81bff976320abbaeeb1b2dcbf
yusuke.akimoto <yusuke.akimoto@ariseanalytics.com>
2024-10-09 06:33:37 +0000

    update documents

--- src/parsers/mod.rs
+++ src/parsers/mod.rs
@@ -3,6 +3,21 @@ pub mod atom;
 pub mod rss1;
 pub mod rss2;
 
+/// Represents a feed with various metadata fields.
+///
+/// This struct is used to deserialize feed data from different formats such as Atom, RSS1, and RSS2.
+///
+/// # Fields
+///
+/// * `title` - The title of the feed.
+/// * `link` - The URL link to the feed.
+/// * `description` - An optional description of the feed.
+/// * `summary` - An optional summary of the feed.
+/// * `updated` - An optional field representing the last updated date of the feed.
+/// * `publish_date` - An optional field representing the publish date of the feed.
+/// * `creator` - An optional field representing the creator of the feed.
+/// * `date` - An optional field representing the date associated with the feed.
+/// * `other` - An optional field for any other additional information.
 #[derive(Deserialize, PartialEq, Debug)]
 pub struct Feed {
     pub title: String,
